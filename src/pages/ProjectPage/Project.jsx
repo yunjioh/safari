0763d1b2aacf8795ce91bce9d7feefc62485679c
yunjioh <dhdbswl08@naver.com>
2026-02-13 21:11:34 +0900

    duration

--- src/pages/ProjectPage/Project.jsx
+++ src/pages/ProjectPage/Project.jsx
@@ -11,7 +11,7 @@ const projectData = [
     subTitle: "영양제 복용 관리 어플",
     mainTitle: "Dr.Pill app",
     decoTitle: "MANAGEMENT APP",
-    duration: "2025.09 - 2025.10",
+    duration: "2025.10.22 - 2025.11.21",
     role: "“이 프로젝트에서는 서비스의 UX 기획부터 최종 디자인까지 전 과정을 수행하였습니다”",
     detail: `더블 다이아몬드 프로세스를 바탕으로 데스크 리서치와 사용자 조사를 통해 문제를 정의하고, 
 페르소나 및 여정 지도를 설계하여 전략적 솔루션을 도출했습니다.
@@ -33,7 +33,7 @@ const projectData = [
     subTitle: "댄서 원밀리언 웹사이트 리뉴얼",
     mainTitle: "1MILLION web",
     decoTitle: "WEBSITE RENEWAL",
-    duration: "2025.11 - 2025.12",
+    duration: "2025.11.26 - 2025.12.28",
     role: "“이 프로젝트에서는 서브 디자이너 및 프론트엔드 메인 코더로서 팀을 지원했습니다”",
     detail: `원밀리언 브랜드의 역동성을 시각화한 메인 페이지 디자인을 수행하고, 
 카드 UI와 디자인 가이드를 구축하여 프로젝트 전반의 시각적 일관성을 확보했습니다. 
@@ -55,7 +55,7 @@ const projectData = [
     subTitle: "버츄얼 팬덤 어플",
     mainTitle: "NOVA app",
     decoTitle: "NOVA APP",
-    duration: "2025.12 - 2026.01",
+    duration: "2025.01.02 - 2026.01.26",
     role: "“이 프로젝트에서는 메인 페이지와 핵심 기능을 디자인하고, 이후 개발에 참여하여 배포하였습니다”",
     detail: `이 프로젝트의 메인 디자이너로서 서비스의 시각 시스템을 총괄하며 메인 페이지와 핵심 기능을 디자인했습니다. 
 직접 설계한 화면을 React 기반의 컴포넌트 단위로 구현하여 디자인 의도가 반영되도록 하였습니다. 
